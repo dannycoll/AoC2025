c480a71bfc3d557387c5a2ed874863fb173a0719
Daniel Collins <dptc98@gmail.com>
2026-01-08 22:06:44 +0000

    day8: missing class

---
+++ app/src/main/java/testjavaproject/Cable.java
@@ -0,0 +1,12 @@
+package testjavaproject;
+
+public class Cable {
+  public int a, b;
+  public long distSq;
+
+  public Cable(long distSq, int a, int b) {
+    this.distSq = distSq;
+    this.a = a;
+    this.b = b;
+  }
+}
